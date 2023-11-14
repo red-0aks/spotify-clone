8cf5f5d45b83437545fbc5e485cd1782eb18edbd
Francisco Sepulveda (JOB) <francisco.sepulveda.job@gmail.com>
2023-11-14 17:34:12 -0300

    Se agregan modelos (Interfaces)

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'app-media-player',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css'],
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover: any = {
+  mockCover: TrackModel = {
     cover: 'https://jenesaispop.com/wp-content/uploads/2009/09/guetta_onelove.jpg',
     album: 'Album',
     name: 'Cancion',
+    url: '',
+    _id: 1
   };
   constructor() {}
 
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { SideBarComponent } from './components/side-bar/side-bar.component';
 import { MediaPlayerComponent } from './components/media-player/media-player.component';
 import { HeaderUserComponent } from './components/header-user/header-user.component';
+import { SectionGenericComponent } from './components/section-generic/section-generic.component';
+import { CardPlayerComponent } from './components/card-player/card-player.component';
 
 
 
@@ -10,15 +12,17 @@ import { HeaderUserComponent } from './components/header-user/header-user.compon
   declarations: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
+    HeaderUserComponent,
+    SectionGenericComponent,
+    CardPlayerComponent,
   ],
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule],
   exports: [
     SideBarComponent,
     MediaPlayerComponent,
-    HeaderUserComponent
-  ]
+    HeaderUserComponent,
+    SectionGenericComponent,
+    CardPlayerComponent,
+  ],
 })
-export class SharedModule { }
+export class SharedModule {}
